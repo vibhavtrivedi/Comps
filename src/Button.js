5b350b985b136026c7fd8712de73f1da43033abb
vibhavtrivedi <vibhavkumar86@gmail.com>
2024-02-04 18:30:04 +0530

    added prop types

--- src/Button.js
+++ src/Button.js
@@ -1,5 +1,24 @@
-function Button({children}) {
+import propTypes from "prop-types";
+function Button({
+  children,
+  primary,
+  secondary,
+  success,
+  warning,
+  danger,
+  outline,
+  rounded
+}) {
   return <button>{children}</button>;
 }
-
+Button.propTypes = {
+  checkVariationValue: ({primary, secondary, success, danger, warning}) => {
+    const count = Number(!!primary) +
+      Number(!!secondary) + Number(!!warning) + Number(!!success)
+      + Number(!!danger)
+    if (count > 1) {
+      return new Error("only one of primary, secondary, danger, warning, success can be true");
+    }
+  }
+}
 export default Button;
