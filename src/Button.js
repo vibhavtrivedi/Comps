9f031fba532c95bf934b4a3b92760b1df52b340d
vibhavtrivedi <vibhavkumar86@gmail.com>
2024-02-07 16:50:17 +0530

    Buttons styling

--- src/Button.js
+++ src/Button.js
@@ -1,4 +1,5 @@
 import className from "classnames";
+import { twMerge } from "tailwind-merge";
 function Button({
   children,
   primary,
@@ -9,13 +10,22 @@ function Button({
   outline,
   rounded,
 }) {
-  const classes = className("px-3 py-1.5 border", {
-    "border-blue-600 bg-blue-500 text-white": primary,
-    "border-gray-900 bg-gray-900 text-white": secondary,
-    "border-green-500 bg-green-500 text-white": success,
-    "border-yellow-400 bg-yellow-400 text-white": warning,
-    "border-red-600 bg-red-500 text-white": danger,
-  });
+  const classes = twMerge(
+    className("flex items-center px-3 py-1.5 border", {
+      "border-blue-500 bg-blue-500 text-white": primary,
+      "border-gray-900 bg-gray-900 text-white": secondary,
+      "border-green-500 bg-green-500 text-white": success,
+      "border-yellow-400 bg-yellow-400 text-white": warning,
+      "border-red-500 bg-red-500 text-white": danger,
+      "rounded-full": rounded,
+      "bg-white": outline,
+      "text-blue-500": outline && primary,
+      "text-gray-900": outline && secondary,
+      "text-green-500": outline && success,
+      "text-yellow-400": outline && warning,
+      "text-red-500": outline && danger,
+    })
+  );
   return <button className={classes}>{children}</button>;
 }
 Button.propTypes = {
