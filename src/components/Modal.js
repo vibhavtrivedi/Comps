1095049b8af8091ed5d9a658c431b9487e93ad02
vibhavtrivedi <vibhavkumar86@gmail.com>
2024-02-26 20:54:04 +0530

    added accept button from parent

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,9 +1,15 @@
 import ReactDOM from "react-dom";
-function Modal({onClose}) {
+function Modal({ onClose, acceptButton, children }) {
   return ReactDOM.createPortal(
     <div>
-      <div onClick={onClose} className="absolute inset-0 bg-gray-300 opacity-80"></div>
-      <div className="absolute inset-40 p-10 bg-white">This is the Modal</div>
+      <div
+        onClick={onClose}
+        className="absolute inset-0 bg-gray-300 opacity-80"
+      ></div>
+      <div className="absolute inset-40 p-10 bg-white">
+        {children}
+        {acceptButton}
+      </div>
     </div>,
     document.querySelector(".modal-container")
   );
--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -10,12 +10,14 @@ function ModalPage() {
   const handleClose = () => {
     setShowModal(false);
   }
+  const acceptButton = <div><Button onClick={handleClose} primary>Accept</Button></div>
+  const modal = <Modal onClose={handleClose} acceptButton={acceptButton}><p>Here is the Important Agreement that you need to Accept!</p></Modal>
   return (
     <div className="relative">
       <Button onClick={handleClick} primary>
         Open Modal
       </Button>
-      {showModal && <Modal onClose={handleClose} />}
+      {showModal && modal}
     </div>
   );
 }
