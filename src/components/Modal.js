c7a91114d4472ce498ddde9a5dc49bd643595b18
vibhavtrivedi <vibhavkumar86@gmail.com>
2024-02-26 21:08:06 +0530

    modal wrapup

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,14 +1,25 @@
+import { useEffect } from "react";
 import ReactDOM from "react-dom";
 function Modal({ onClose, acceptButton, children }) {
+    useEffect(() => {
+        document.body.classList.add('overflow-hidden');
+        return (() => {
+            document.body.classList.remove('overflow-hidden');
+        })
+    },[])
   return ReactDOM.createPortal(
     <div>
       <div
         onClick={onClose}
-        className="absolute inset-0 bg-gray-300 opacity-80"
+        className="fixed inset-0 bg-gray-300 opacity-80"
       ></div>
-      <div className="absolute inset-40 p-10 bg-white">
-        {children}
-        {acceptButton}
+          <div className="fixed inset-40 p-10 bg-white">
+              <div className="flex flex-col justify-between h-full">
+                  {children}
+                  <div className="flex justify-end">
+                    {acceptButton}
+                  </div>
+              </div>
       </div>
     </div>,
     document.querySelector(".modal-container")
