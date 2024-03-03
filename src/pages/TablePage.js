6852b56dcce320463dba7370ee9f56d60c1d6400
vibhavtrivedi <vibhavkumar86@gmail.com>
2024-03-03 10:25:07 +0530

    sort table changes

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,4 +1,4 @@
-import Table from "../components/Table";
+import SorttableTable from "../components/SorttableTable";
 
 function TablePage() {
   const data = [
@@ -9,7 +9,7 @@ function TablePage() {
   ];
 
   const config = [
-    { label: "Fruits", render: (fruit) => fruit.name },
+    { label: "Fruits", render: (fruit) => fruit.name, sortValue: (fruit) => fruit.name },
     {
       label: "Color",
       render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`} />,
@@ -18,6 +18,7 @@ function TablePage() {
       label: "Score",
       render: (fruit) => fruit.score,
       header: () => <th className="bg-red-500">Score</th>,
+      sortValue: (fruit) => fruit.score
     },
   ];
 
@@ -26,7 +27,7 @@ function TablePage() {
   };
   return (
     <div>
-      <Table data={data} config={config} keyFn={keyFn} />
+      <SorttableTable data={data} config={config} keyFn={keyFn} />
     </div>
   );
 }
